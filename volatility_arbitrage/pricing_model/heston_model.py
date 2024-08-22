ec14605907b2abc3dc67f9b83c51d3c556bd2a98
Junsu Park <batman823@berkeley.edu>
2024-08-22 22:21:08 +0200

    initial

--- volatility_arbitrage/pricing_model/heston_model.py
+++ volatility_arbitrage/pricing_model/heston_model.py
@@ -7,25 +7,25 @@ from typing import Tuple, Union
 import numpy as np
 import numpy.typing as npt
 
-from volatility_arbitrage.pricing_model.interface import Correlation, HestonParams
+from volatility_arbitrage.pricing_model.interface import HestonParams, MarketModel
 
 ARRAY = npt.NDArray[np.float64]
 
 
-def generate_initial_var(var_params: HestonParams, size: Union[int, Tuple[int, ...]]) -> ARRAY:
+def generate_initial_var(model_params: HestonParams, size: Union[int, Tuple[int, ...]]) -> ARRAY:
     """
-    :param var_params: Heston parameters
+    :param model_params: Heston parameters
     :param size: size
     :return: samples that follow the asymptotice distribution of CIR process
     """
-    alpha = 2 * var_params.kappa * var_params.mean_of_var / var_params.vol_of_var**2
-    beta = 2 * var_params.kappa / var_params.vol_of_var**2
+    alpha = 2 * model_params.kappa * model_params.mean_of_var / model_params.vol_of_var**2
+    beta = 2 * model_params.kappa / model_params.vol_of_var**2
     return np.random.gamma(shape=alpha, scale=1 / beta, size=size)
 
 
 def generate_cir_processs(
     var_0: float,
-    var_params: HestonParams,
+    model_params: HestonParams,
     normal_var: ARRAY,
     num_path: int,
     length: int,
@@ -35,23 +35,23 @@ def generate_cir_processs(
     Kahl, C., & Jäckel, P. (2006). Fast strong approximation Monte Carlo schemes for stochastic volatility models. Quantitative Finance, 6(6), 513-536.
 
     :param var_0: initial variance
-    :param var_params: Heston paremters
+    :param model_params: Heston paremters
     :param normal_var: i.i.d. standard normal random
     :param num_path: number of paths
     :param length: length of a path
     :param time_delta: time delta in years
     :return: Simulate CIR process
     """
-    # initial_var = generate_initial_var(var_params=var_params, size=1)
+    # initial_var = generate_initial_var(model_params=model_params, size=1)
 
     var = np.zeros(shape=(length + 1, num_path))
     var[0] = var_0
 
-    drift = var_params.kappa * var_params.mean_of_var * time_delta
-    mean_reversion_adj = 1 / (1 + var_params.kappa * time_delta)
-    milstein_adj = 0.25 * var_params.vol_of_var**2 * (normal_var**2 - 1) * time_delta
+    drift = model_params.kappa * model_params.mean_of_var * time_delta
+    mean_reversion_adj = 1 / (1 + model_params.kappa * time_delta)
+    milstein_adj = 0.25 * model_params.vol_of_var**2 * (normal_var**2 - 1) * time_delta
     for i in range(length):
-        diffusion = var_params.vol_of_var * np.sqrt(var[i]) * normal_var[i] * np.sqrt(time_delta)
+        diffusion = model_params.vol_of_var * np.sqrt(var[i]) * normal_var[i] * np.sqrt(time_delta)
         var[i + 1] = (
             np.maximum(var[i] + drift + diffusion + milstein_adj[i], 0) * mean_reversion_adj
         )
@@ -61,10 +61,9 @@ def generate_cir_processs(
 
 def generate_heston_processes(
     var_0: float,
-    var_params: HestonParams,
+    model_params: HestonParams,
     normal_var_1: ARRAY,
     normal_var_2: ARRAY,
-    rho: float,
     num_path: int,
     length: int,
     time_delta: float,
@@ -73,16 +72,15 @@ def generate_heston_processes(
     Kahl, C., & Jäckel, P. (2006). Fast strong approximation Monte Carlo schemes for stochastic volatility models. Quantitative Finance, 6(6), 513-536.
 
     :param var_0: initial variance
-    :param var_params: Heston paremters
+    :param model_params: Heston paremters
     :param normal_var_1: i.i.d. standard normal random vector
     :param normal_var_2: i.i.d. standard normal random vector
-    :param rho: corr bewteen normal var 1 and normal var 2
     :param num_path: number of paths
     :param length: length of a path
     :param time_delta: time delta in years
     :return: log return and instantaneous variance
     """
-    var = generate_cir_processs(var_0, var_params, normal_var_1, num_path, length, time_delta)
+    var = generate_cir_processs(var_0, model_params, normal_var_1, num_path, length, time_delta)
     vol = np.sqrt(var)
     lr = np.zeros((length + 1, num_path))
 
@@ -90,9 +88,13 @@ def generate_heston_processes(
     avg_vol = 0.5 * (vol[:-1] + vol[1:])
 
     drift = -0.5 * avg_var * time_delta
-    corr_diffusion = rho * vol[:-1] * normal_var_1 * np.sqrt(time_delta)
-    uncorr_diffusion = np.sqrt(1 - rho**2) * avg_vol * normal_var_2 * np.sqrt(time_delta)
-    milstein_correction = 0.5 * rho * var_params.vol_of_var * (normal_var_2**2 - 1) * time_delta
+    corr_diffusion = model_params.rho * vol[:-1] * normal_var_1 * np.sqrt(time_delta)
+    uncorr_diffusion = (
+        np.sqrt(1 - model_params.rho**2) * avg_vol * normal_var_2 * np.sqrt(time_delta)
+    )
+    milstein_correction = (
+        0.5 * model_params.rho * model_params.vol_of_var * (normal_var_2**2 - 1) * time_delta
+    )
     lr[1:] = drift + corr_diffusion + uncorr_diffusion + milstein_correction
     return lr, var
 
@@ -101,9 +103,7 @@ def generate_inefficient_market(
     *,
     real_var_0: float,
     imp_var_0: float,
-    real_var_params: HestonParams,
-    imp_var_params: HestonParams,
-    corr: Correlation,
+    market_model: MarketModel,
     num_path: int,
     length: int,
     time_delta: float,
@@ -111,9 +111,7 @@ def generate_inefficient_market(
     """
     :param real_var_0: initial realized variance
     :param imp_var_0: initial implied variance
-    :param real_var_params: realzied Heston paremters
-    :param imp_var_params: implied Heston paremters
-    :param corr: correlation matrix
+    :param market_model: market_model
     :param num_path: number of paths
     :param length: length of a path
     :param time_delta: time delta in years
@@ -122,21 +120,20 @@ def generate_inefficient_market(
     normal_var = np.random.normal(size=(3, length, num_path))
     lr, real_var = generate_heston_processes(
         var_0=real_var_0,
-        var_params=real_var_params,
+        model_params=market_model.real_model,
         normal_var_1=normal_var[0],
         normal_var_2=normal_var[1],
-        rho=corr.rho_spot_real,
         num_path=num_path,
         length=length,
         time_delta=time_delta,
     )
-
+    cholesky = market_model.cholesky()
     correlated_normal = (
-        corr.cholesky[2][0] * normal_var[0]
-        + corr.cholesky[2][1] * normal_var[1]
-        + corr.cholesky[2][2] * normal_var[2]
+        cholesky[2][0] * normal_var[0]
+        + cholesky[2][1] * normal_var[1]
+        + cholesky[2][2] * normal_var[2]
     )
     imp_var = generate_cir_processs(
-        imp_var_0, imp_var_params, correlated_normal, num_path, length, time_delta
+        imp_var_0, market_model.imp_model, correlated_normal, num_path, length, time_delta
     )
     return lr, real_var, imp_var
--- volatility_arbitrage/pricing_model/interface.py
+++ volatility_arbitrage/pricing_model/interface.py
@@ -1,6 +1,6 @@
 """interface of pricing model"""
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import TypedDict
 
 import numpy as np
@@ -9,50 +9,50 @@ import numpy.typing as npt
 ARRAY = npt.NDArray[np.float64]
 
 
-@dataclass
+@dataclass(frozen=True)
 class HestonParams:
-    """Heston stochastic volatilty parameters"""
+    """Heston stochastic volatilty model parameters"""
 
     kappa: float
     mean_of_var: float
     vol_of_var: float
+    rho: float
 
     def __post_init__(self) -> None:
         assert self.mean_of_var > 0
         assert self.kappa > 0
         assert self.vol_of_var > 0
+        assert abs(self.rho) < 1
         # Check feller condition
         assert 2 * self.kappa * self.mean_of_var > self.vol_of_var**2
 
 
-@dataclass
-class Correlation:
-    """Correlation among spot price, implied variance, and realized variance processes"""
+@dataclass(frozen=True)
+class MarketModel:
+    """Market model"""
 
-    rho_spot_imp: float
-    rho_spot_real: float
-    rho_real_imp: float
-
-    cholesky: ARRAY = field(init=False, repr=False)
+    imp_model: HestonParams
+    real_model: HestonParams
+    rho_spot_imp_var: float
+    rho_real_var_imp_var: float
 
     def __post_init__(self) -> None:
-        for field_name, value in self.__dict__.items():
-            if 1 < abs(value):
-                raise ValueError(
-                    f"Value of '{field_name}' must be between -1 and 1. Got {value} instead."
-                )
+        assert abs(self.rho_spot_imp_var) < 1
+        assert abs(self.rho_real_var_imp_var) < 1
 
+    def cholesky(self) -> ARRAY:
+        """Cholesky decomposition"""
         # Construct the correlation matrix
         corr_matrix = np.array(
             [
-                [1.0, self.rho_spot_real, self.rho_real_imp],
-                [self.rho_spot_real, 1.0, self.rho_spot_imp],
-                [self.rho_real_imp, self.rho_spot_imp, 1.0],
+                [1.0, self.real_model.rho, self.rho_real_var_imp_var],
+                [self.real_model.rho, 1.0, self.rho_spot_imp_var],
+                [self.rho_real_var_imp_var, self.rho_spot_imp_var, 1.0],
             ]
         )
 
         try:
-            self.cholesky = np.linalg.cholesky(corr_matrix)
+            return np.linalg.cholesky(corr_matrix)
         except np.linalg.LinAlgError as exc:
             raise ValueError("The correlation matrix is not positive definite.") from exc
 
@@ -68,7 +68,7 @@ class StrategyPnl(TypedDict):
     vega_hedge_pnl: ARRAY
 
 
-@dataclass
+@dataclass(frozen=True)
 class StrategyPnlCalculator:
     """strategy pnl calculator"""
 
